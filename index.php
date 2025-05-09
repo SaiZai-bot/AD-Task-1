36d0e0a0a12a109078e27d865e4e439cf21a536e
Don Zaimone O. Miranda <saimone.don@gmail.com>
2025-05-09 08:54:54 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,11 +7,17 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>THIS IS MY HEADER, MATH Lesson</h1>
-    <p>This will be addition, subtraction, multiplication, and division</p>
-    <a href="quiz.php">
-        <button>QUIZ</button>
-    </a>
+    <?php 
+        echo "<div class='container'>";
+        echo "<h1>Welcome to Math Wizards</h1>";
+        echo "<p>Ready to boost your math skills and become a math wizard? This math lesson will guide you on the basics of mathematics and a little extra learning to master Mathematics</p>";
+        echo "<p>If you are ready to become a Math Wizard just click the button!!</p>";
+        echo "<div class ='button-container'>";
+        echo "<a href ='quiz.php'><button>Let's Get Started</button></a>";
+        echo "</div>";
+        echo "</div>";
+    ?>
+    
 </body>
 </html>
 
--- quiz.php
+++ quiz.php
@@ -51,40 +51,44 @@ Looping -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math Lesson</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="quiz.css">
 </head>
 <body>
     <?php
+        echo "<div class='group operator'>";
         echo "<div class='container'>";
-        echo "<div class='title'>Addition</div>";
+        echo "<div class='title'> ➕ Addition</div>";
         for($i = 0; $i < count($Addition); $i++){
             echo "<p>" . $Addition[$i][0] . " = " . $Addition[$i][1] . "</p>";
         }
         echo "</div>";
         
         echo "<div class='container'>";
-        echo "<div class='title'>Subtraction</div>";
+        echo "<div class='title'> ➖ Subtraction</div>";
         for($i = 0; $i < count($Subtraction); $i++){
             echo "<p>".$Subtraction[$i][0] . " = " . $Subtraction[$i][1] . "</p>";
         }
         echo "</div>";
 
         echo "<div class ='container'>";
-        echo "<div class  ='title'>Multiplication</div>";
+        echo "<div class  ='title'> ✖️ Multiplication</div>";
         for($i = 0; $i < count($Multiplication); $i++){
             echo "<p>".$Multiplication[$i][0] . " = " . $Multiplication[$i][1] . "</p>";
         }
         echo "</div>";
 
         echo "<div class ='container'>";
-        echo "<div class ='title'>Division</div>";
+        echo "<div class ='title'> ➗ Division</div>";
         for($i = 0; $i < count($Division); $i++){
             echo "<p>" . $Division[$i][0] . " = " . $Division[$i][1] . "</p>";
         }
         echo "</div>";
 
+        echo "</div>";
+
+        echo "<div class='group comparison'>";
         echo "<div class ='container'>";
-        echo "<div class ='title'>Greater Than</div>";
+        echo "<div class ='title'> 🔼 Greater Than</div>";
         for($i = 0; $i < count($Greaterthan); $i++){
             if($Greaterthan[$i][0] > $Greaterthan[$i][1]) {
                 echo "<p>". $Greaterthan[$i][0] . " is Greater than " . $Greaterthan[$i][1] . "</p>";
@@ -97,7 +101,7 @@ Looping -->
 
 
         echo "<div class='container'>";
-        echo "<div class ='title'>Equal To </div>";
+        echo "<div class ='title'> 🟰 Equal To </div>";
         for($i = 0; $i < count($Equalto); $i++){
             if($Equalto[$i][0] == $Equalto[$i][1]) {
                 echo "<p>" . $Equalto[$i][0] . " is Equal to " . $Equalto[$i][1] . "</p>";
@@ -110,7 +114,7 @@ Looping -->
 
 
         echo "<div class='container'>";
-        echo "<div class = 'title'>Less Than</div>";
+        echo "<div class = 'title'> 🔽 Less Than</div>";
         for($i = 0; $i < count($Lessthan); $i++){
             if($Lessthan[$i][0] < $Lessthan[$i][1]) {
                 echo "<p>".$Lessthan[$i][0] . " is Less than " . $Lessthan[$i][1] . "</p>";
@@ -122,6 +126,7 @@ Looping -->
 
         echo "</div>";
 
+        echo "</div>";
     ?>
     <a href="index.php">
         <button>Go Back</button>
